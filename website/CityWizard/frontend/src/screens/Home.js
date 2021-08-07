34d245e43a53fa6ee66415c1272f95c37a2e292e
MimiKing01 <milanj1705@gmail.com>
2021-08-07 03:25:53 +0200

    Video fixed

--- website/CityWizard/frontend/src/screens/Home.js
+++ website/CityWizard/frontend/src/screens/Home.js
@@ -1,13 +1,13 @@
 import { translate } from 'ol/transform';
 import { Button, Container, Row } from 'react-bootstrap';
 import Search from '../components/Search';
-import vid from '../media/background.mp4';
+import vid from '../media/background-1.mp4';
 
 export default function Home() {
     return (
         <div>
-            <Row className="vh-100" style={{ overflow: "hidden", overflowY: "hidden" }}>
-                <video autoPlay loop muted style={{ position: "absolute", zIndex: "-2", height: "100%", width: "100%", }}>
+            <Row className="vh-100" style={{ overflow: "hidden", overflowY: "hidden", margin: 0 }}>
+                <video autoPlay loop muted style={{ position: "absolute", zIndex: "-2", height: "100%", width: "100%", objectFit: "cover", padding: 0 }}>
                     <source src={vid} type="video/mp4"></source>
                 </video>
                 <div id="login">
