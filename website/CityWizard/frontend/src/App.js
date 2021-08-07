cf913a69557195e195c5bdfce3df6674adf71625
gase273 <39773535+gase273@users.noreply.github.com>
2021-08-07 12:05:56 +0200

    added login, register screens

--- website/CityWizard/frontend/src/App.js
+++ website/CityWizard/frontend/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./screens/Home.js"
+import Login from "./screens/Login.js"
+import Register from "./screens/Register"
 
 function App() {
   return (
---
+++ website/CityWizard/frontend/src/screens/Login.js
@@ -0,0 +1,26 @@
+import { Button, Form } from "react-bootstrap";
+
+export default function Login() {
+  return (
+    <div>
+      <div id="loginForm">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
+}
